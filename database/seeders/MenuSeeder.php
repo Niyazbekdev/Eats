6153f893e0eca9e298fab81f1a63c3ca871d0db3
Niyazbekdev <niyazbekk001@gmail.com>
2023-10-04 19:52:04 +0500

    Menu ( model, migration, seeder)

---
+++ database/seeders/MenuSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MenuSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $menus = [
+            [
+                'name' => 'Salad',
+                'image' => 'salad.jpeg'
+            ],
+            [
+                'name' => 'soap',
+                'image' => 'soap.jpeg'
+            ]
+        ];
+        DB::table('menus')->insert($menus);
+    }
+}
