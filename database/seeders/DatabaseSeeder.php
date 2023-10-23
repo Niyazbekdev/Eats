155f90ee54a0cedec3510011e0ea2bb05e6669e2
Niyazbekdev <niyazbekk001@gmail.com>
2023-10-23 21:22:43 +0500

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(
             [
-                MenuSeeder::class,
+                AMenuSeeder::class,
                 FoodSeeder::class,
             ]
         );
